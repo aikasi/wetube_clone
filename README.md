e89605695b7cc2e7d6a0552df6d9f369a6975934
aikasi <32337856+aikasi@users.noreply.github.com>
2021-01-19 11:37:27 +0900

    express_view

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
  # WeTube
 
  Cloning Youtube with Vanilla and NodeJS
+
+ ## Pages:
+ - [ ] Home
+ - [x] Join
+ - [x] Login
+ - [x] Search
+ - [ ] userDetail
+ - [x] editProfile
+ - [x] changePassword
+ - [x] Upload
+ - [ ] Video Detail
+ - [x] Edit Video
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 123123,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "http://media.w3.org/2010/05/sintel/trailer.webm",
+    creator: {
+      id: 12312312,
+      name: "aaaaaa",
+      email: "aaaa@aaa.com",
+    },
+  },
+  {
+    id: 123444,
+    title: "Video Super",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "http://media.w3.org/2010/05/sintel/trailer.webm",
+    creator: {
+      id: 12312312,
+      name: "aaaaaa",
+      email: "aaaa@aaa.com",
+    },
+  },
+  {
+    id: 5511122,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "http://media.w3.org/2010/05/sintel/trailer.webm",
+    creator: {
+      id: 12312312,
+      name: "aaaaaa",
+      email: "aaaa@aaa.com",
+    },
+  },
+  {
+    id: 34563245,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "http://media.w3.org/2010/05/sintel/trailer.webm",
+    creator: {
+      id: 12312312,
+      name: "aaaaaa",
+      email: "aaaa@aaa.com",
+    },
+  },
+];
